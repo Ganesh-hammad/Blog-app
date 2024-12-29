5434d08363729ad7ba099cbd961abec9ad8e5b04
Ganesh Hammad <ganeshhammad837@gmail.com>
2024-12-29 17:45:28 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,6 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB:", error);
+  });
 
 const app = express();
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port: " + PORT);
 });
