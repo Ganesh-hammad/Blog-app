5c965cdd7684e41e4b48c4ea2add179a923ec4a6
Ganesh Hammad <ganeshhammad837@gmail.com>
2024-12-30 18:57:07 +0530

    create signup api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -15,9 +16,11 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
 });
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
