4f6296b4a5f8f67cfbd302476d877fb6f1c14fbe
Ganesh Hammad <ganeshhammad837@gmail.com>
2024-12-30 21:00:42 +0530

    add a middileware fuction to handle errores

--- api/index.js
+++ api/index.js
@@ -24,3 +24,13 @@ app.listen(PORT, () => {
 });
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
+app.use((error,req, res, next) => {
+  const statusCode = error.statusCode || 500;
+  const message = error.message || " Inrternal server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  });
+});
