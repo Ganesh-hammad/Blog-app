ed80dab88554189e5ca163540c6b69b16353487e
Ganesh Hammad <ganeshhammad837@gmail.com>
2025-01-02 17:52:17 +0530

    create redux-toolkit  strore

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,15 @@ import { createRoot } from 'react-dom/client'
 import 'flowbite/dist/flowbite.min.css';
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux';
+import { store, persistor } from './reduxtoolkit/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  </PersistGate>
 )
