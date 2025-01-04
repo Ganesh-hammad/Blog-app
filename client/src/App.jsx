c0eaec327ad95b1576641cdc43e686eb443d74ea
Ganesh Hammad <ganeshhammad837@gmail.com>
2025-01-04 15:30:05 +0530

    create dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
 import FooterComponent from './components/FooterComponent'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
@@ -17,10 +18,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/projects" element={<Project/>} />
         <Route path="/signin" element={<SignIn/>} />
         <Route path="/signup" element={<SignUp/>} />
+        <Route element={<PrivateRoute/>}>
+           <Route path="/dashboard" element={<Dashboard/>} />
+        </Route>
+        <Route path="/projects" element={<Project/>} />
       </Routes>
       <FooterComponent/>
       </BrowserRouter>
