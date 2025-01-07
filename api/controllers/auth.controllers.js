497135cf0acb4bfef79fe99211c4afe57e103621
Ganesh Hammad <ganeshhammad837@gmail.com>
2025-01-07 15:22:49 +0530

    add admin fuctionality to the user

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -44,7 +44,7 @@ export const signin = async (req, res, next) => {
         
         // npm install jsonwebtoken
         const token = jwt.sign(
-            { id: validUser._id, isAdmin: validUser._isAdmin }, process.env.JWT_SECRET_KEY);
+            { id: validUser._id, isAdmin: validUser.isAdmin }, process.env.JWT_SECRET_KEY);
             const {password:pass, ...rest} = validUser._doc;
             res
             .status(200)
